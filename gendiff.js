1a632e52652bbfa1c4859e8af897434a67d2e689
Semikx-x <semen.samsonov@mail.ru>
2025-06-03 16:24:49 +0300

    AddedFormat

--- gendiff.js
+++ gendiff.js
@@ -6,7 +6,8 @@ const gendiff = new Command();
 
 gendiff
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1');
+  .version('0.0.1')
+  .option('-f, --format [type]', 'output format');
 
 gendiff.parse(process.argv);
 
