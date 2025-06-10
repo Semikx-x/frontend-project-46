162df39dc341bf199f79a50464be08c554109212
Semikx-x <semen.samsonov@mail.ru>
2025-06-10 19:18:06 +0300

    fix

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -4,18 +4,22 @@ const formatValue = (value) => {
   return String(value)
 }
 
+function getDisplayValue(value) {
+  return typeof (value) == String ? `'${value}'` : value
+}
+
 const formatPlain = (diff, path = []) => {
   const lines = diff.flatMap((node) => {
     const propertyPath = [...path, node.key].join('.')
     switch (node.type) {
       case 'added':
-        return `Property '${propertyPath}' was added with value: '${formatValue(node.value)}'`
+        return `Property '${propertyPath}' was added with value: ${getDisplayValue(formatValue(node.value))}`
       case 'removed':
         return `Property '${propertyPath}' was removed`
       case 'unchanged':
         return []
       case 'changed':
-        return `Property '${propertyPath}' was updated. From '${formatValue(node.oldValue)}' to '${formatValue(node.newValue)}'`
+        return `Property '${propertyPath}' was updated. From ${getDisplayValue(formatValue(node.oldValue))} to ${getDisplayValue(formatValue(node.newValue))}`
       case 'nested':
         return formatPlain(node.children, [...path, node.key])
     }
